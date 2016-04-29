3bde4133b6df29db636966bb6da0d0f5588602a3
Chris Jaynes <chris@jaynes.me>
2016-04-28 22:31:34 -0500

    add home, follow camera, better gui

---
+++ Assets/HideGuiByScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HideGuiByScene : MonoBehaviour {
+
+  public static string playerScene = "";
+
+  Canvas canvas;
+
+  void Awake(){
+    canvas = GetComponent<Canvas>();
+  }
+
+	void Update () {
+    Debug.Log(playerScene);
+    var enabled = (playerScene == "quiz-room");
+    canvas.enabled = enabled;
+	}
+}
---
+++ Assets/PlayerMoveDetector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMoveDetector : MonoBehaviour {
+
+  public void OnPlayerMove(object playAreaName){
+    Debug.Log("OnPlayerMove: " + playAreaName.ToString());
+    HideGuiByScene.playerScene = playAreaName.ToString();
+  }
+}
