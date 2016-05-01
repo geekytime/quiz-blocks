a16582af185a46cc33c836fbac8a9db2242ee44b
Chris Jaynes <chris@jaynes.me>
2016-04-30 21:55:31 -0500

    persist hats

--- Assets/Inventory.cs
+++ Assets/Inventory.cs
@@ -4,19 +4,22 @@ using System.Collections.Generic;
 public class Inventory {
 
   static Inventory instance;
-  static List<string> hats = new List<string>();
+  static List<string> hats;
 
   public static Inventory GetInstance(){
-    if (instance == null){
+    if (instance == null){      
       instance = new Inventory();
+      instance.LoadCoins();
+      instance.LoadHats();
     }
     return instance;
   }
 
-  int coins = 100;
+  int coins = 0;
 
   public void AddCoin(){
     coins = coins + 1;
+    SaveCoins();
   }
 
   public int GetCoinCount(){
@@ -30,17 +33,45 @@ public class Inventory {
     }
 
     coins = coins - cost;
+    SaveCoins();
     if (itemType == "hat")
     {       
       hats.Add(itemKey);
       ActivateHat(itemKey);
+      SaveHats();
     }
     return true;
+  }
+
+  public bool HasItem(string itemType, string itemKey){
+    if (itemType == "hat")
+    {
+      return hats.Contains(itemKey);
+    }
+    return false;
   }
 
   void ActivateHat(string hatName){
     var hats = GameObject.Find("player-hats").GetComponent<HatsController>();
     hats.Wear(hatName);
   }
+
+  void SaveCoins(){
+    PlayerPrefs.SetInt("coins", coins);
+    PlayerPrefs.Save();
+  }
+
+  void LoadCoins(){
+    coins = PlayerPrefs.GetInt("coins");
+  }
+
+  void SaveHats(){
+    PlayerPrefsEx.SetStringArray("hats", hats.ToArray());
+  }
+
+  void LoadHats(){
+    hats = new List<string>(PlayerPrefsEx.GetStringArray("hats"));
+  }
+
     	
 }
--- Assets/ShopItem.cs
+++ Assets/ShopItem.cs
@@ -8,6 +8,7 @@ public class ShopItem : UsableActivated {
   public int Cost = 100;
   public string ItemType = "item";
   public string ItemKey = "item";
+  bool available = true;
 
   TextMesh costText;
   SpriteRenderer icon;
@@ -17,14 +18,27 @@ public class ShopItem : UsableActivated {
     costText.text = Cost.ToString();
 
     icon = transform.FindChild("icon").GetComponent<SpriteRenderer>();
+
+    if (Inventory.GetInstance().HasItem(ItemType, ItemKey))
+    {
+      Hide();
+    }
   }
 
-  public override void Activate(GameObject player){    
-    var success = Inventory.GetInstance().BuyItem(Cost, ItemType, ItemKey);
-    if (success)
+  public override void Activate(GameObject player){
+    if (available)
     {
-      costText.gameObject.SetActive(false);
-      icon.gameObject.SetActive(false);
+      var success = Inventory.GetInstance().BuyItem(Cost, ItemType, ItemKey);
+      if (success)
+      {
+        Hide();
+      }
     }
+  }
+
+  void Hide(){
+    available = false;
+    costText.gameObject.SetActive(false);
+    icon.gameObject.SetActive(false);
   }
 }
