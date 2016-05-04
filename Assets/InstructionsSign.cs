c325afd872fc4d18d0aee9a0dc05acd16993f4b1
Chris Jaynes <chris@jaynes.me>
2016-05-03 23:33:39 -0500

    add instructions

---
+++ Assets/InstructionsSign.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsSign : MonoBehaviour {
+	
+  public GameObject Instructions;
+  public GameObject Text;
+
+  BoxCollider2D sensor;
+  Collider2D[] colliders = new Collider2D[5];
+
+	void Start () {
+    sensor = GetComponent<BoxCollider2D>();	
+	}
+		
+	void Update () {    
+    Physics2D.OverlapAreaNonAlloc(sensor.bounds.min, sensor.bounds.max, colliders);
+    foreach (var collider in colliders)
+    {
+      if (collider && collider.name == "bez")
+      {
+        Instructions.SetActive(true);
+        Text.SetActive(false);
+        return;
+      }
+    }
+    Instructions.SetActive(false);
+    Text.SetActive(true);
+	}
+}
